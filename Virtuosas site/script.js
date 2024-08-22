76d811649b558e4a74748272bca51ff40b21dd18
Nicole Trindade <45500884+nickitrin@users.noreply.github.com>
2024-08-22 19:41:08 -0300

    Add files via upload

---
+++ Virtuosas site/script.js
@@ -0,0 +1,32 @@
+// Define a data alvo
+const dataAlvo = new Date('September 15, 2024 07:00:00').getTime();
+
+// Atualiza o contador a cada segundo
+const intervalo = setInterval(function() {
+    const agora = new Date().getTime();
+    const diferenca = dataAlvo - agora;
+
+    // Cálculo do tempo restante
+    const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((diferenca % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString().padStart(2, '0');
+    const minutos = Math.floor((diferenca % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, '0');
+    const segundos = Math.floor((diferenca % (1000 * 60)) / 1000).toString().padStart(2, '0');
+    
+    // Exibe o resultado no elemento com id="contadordias"
+    document.getElementById('contadordias').innerHTML = `${dias} DIAS`;
+    document.getElementById('contador').innerHTML = `${horas}:${minutos}:${segundos}`;
+    
+    // Se a contagem terminar, exibe uma mensagem
+    if (diferenca < 0) {
+        clearInterval(intervalo);
+        document.getElementById('contador').innerHTML = "Contagem finalizada!";
+    }
+}, 1000);
+
+let porcentagem = 90;
+
+// Espera o carregamento do conteúdo da página antes de iniciar a animação
+window.onload = function() {
+  // Aplica a porcentagem ao estilo da barra com a animação
+  document.getElementById('barra').style.width = `${porcentagem}%`;
+};
